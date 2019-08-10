487ebb128e800c53b2b3c24ffbd017b8919bfd18
Janak <doon.29292@gmail.com>
2019-08-10 16:53:04 +0530

    check for client

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class App extends Component {
+
+  state = {
+    name: '',
+    image: null,
+    token: '',
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  };
+
+  handleImageChange = (e) => {
+    this.setState({
+      image: e.target.files[0]
+    })
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    let form_data = new FormData();
+    form_data.append('image', this.state.image, this.state.image.name);
+    form_data.append('name', this.state.name);
+    let url = 'http://localhost:8000/api/client/';
+    axios.post(url, form_data, {
+      headers: {
+        'content-type': 'multipart/form-data',
+        'Authorization' : 'JWT '+ this.state.token
+      }
+    })
+        .then(res => {
+          console.log(res.data);
+        })
+        .catch(err => console.log(err))
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <input type="text" placeholder='JWT Token' id='token' value={this.state.token} onChange={this.handleChange} required/>
+          </p>
+          <p>
+            <input type="text" placeholder='Name' id='name' value={this.state.name} onChange={this.handleChange} required/>
+          </p>
+          <p>
+            <input type="file"
+                   id="image"
+                   accept="image/png, image/jpeg"  onChange={this.handleImageChange} required/>
+          </p>
+          <input type="submit"/>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
