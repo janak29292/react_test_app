bb27fe3363317be534501ddc500bfa0e10382bcb
Janak <doon.29292@gmail.com>
2019-08-12 17:41:31 +0530

    Pitch create with list of data

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ class App extends Component {
   state = {
     name: '',
     doc: null,
-    images: null,
+    image1: null,
+    image2: null,
     token: '',
     value: [],
-    topics: ["http://0.0.0.0:8000/api/interest/2/"],
-    cta: ["interview"]
+    topics: [],
+    cta: "interview"
   };
 
   handleChange = (e) => {
@@ -38,11 +39,14 @@ class App extends Component {
     console.log(this.value);
     let form_data = new FormData();
     form_data.append('press_release', this.state.doc, this.state.doc.name);
-    form_data.append('images', this.state.images, this.state.images.name);
+    form_data.append('images', this.state.image1, this.state.image1.name);
+    form_data.append('images', this.state.image2, this.state.image2.name);
     form_data.append('title', this.state.name);
     form_data.append('client', 'http://0.0.0.0:8000/api/client/1/')
-    form_data.append("topics", this.state.topics )
-    form_data.append("cta", this.state.cta )
+    form_data.append("topics", "http://0.0.0.0:8000/api/interest/2/" )
+    form_data.append("topics", "http://0.0.0.0:8000/api/interest/1/" )
+    form_data.append("cta", "interview")
+    form_data.append("cta", "coverage")
     form_data.append('content', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
     form_data.append('availability', 'regular')
     let url = 'http://localhost:8000/api/pitch/';
@@ -66,13 +70,19 @@ class App extends Component {
             <input type="text" placeholder='Title' id='name' value={this.state.name} onChange={this.handleChange} required/>
           </p>
           <p>
+          <label for="doc">DOC</label>
             <input type="file"
                    id="doc"
                   onChange={this.handleImageChange} required/>
           </p>
           <p>
+          <label for="image1">Image1</label>
             <input type="file"
-                   id="images"
+                   id="image1"
+                  onChange={this.handleImageChange} required/>
+          <label for="image2">Image1</label>
+            <input type="file"
+                   id="image2"
                   onChange={this.handleImageChange} required/>
           </p>
           <input type="submit"/>
