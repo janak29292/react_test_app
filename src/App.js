3db8768eac5dd0ead35f2d6252661d8f58bc2c98
Janak <doon.29292@gmail.com>
2019-08-12 15:43:37 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ class App extends Component {
 
   state = {
     name: '',
-    image: null,
+    doc: null,
+    images: null,
     token: '',
-    value: []
+    value: [],
+    topics: ["http://0.0.0.0:8000/api/interest/2/"],
+    cta: ["interview"]
   };
 
   handleChange = (e) => {
@@ -25,21 +28,28 @@ class App extends Component {
 
   handleImageChange = (e) => {
     this.setState({
-      image: e.target.files[0]
+      [e.target.id]: e.target.files[0]
     })
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
     console.log(this.state);
+    console.log(this.value);
     let form_data = new FormData();
-    form_data.append('image', this.state.image, this.state.image.name);
-    form_data.append('name', this.state.name);
-    let url = 'http://localhost:8000/api/client/';
+    form_data.append('press_release', this.state.doc, this.state.doc.name);
+    form_data.append('images', this.state.images, this.state.images.name);
+    form_data.append('title', this.state.name);
+    form_data.append('client', 'http://0.0.0.0:8000/api/client/1/')
+    form_data.append("topics", this.state.topics )
+    form_data.append("cta", this.state.cta )
+    form_data.append('content', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+    form_data.append('availability', 'regular')
+    let url = 'http://localhost:8000/api/pitch/';
     axios.post(url, form_data, {
       headers: {
         'content-type': 'multipart/form-data',
-        'Authorization' : 'JWT '+ this.state.token
+        'Authorization' : 'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6InByQGdtYWlsLmNvbSIsImV4cCI6MTU2ODE4ODc0MywiZW1haWwiOiJwckBnbWFpbC5jb20iLCJvcmlnX2lhdCI6MTU2NTU5Njc0M30.-FdcW4L26_xGRDhSEXRxXDxPjaEVnErJtsXrfhfwoiw'
       }
     })
         .then(res => {
@@ -53,23 +63,17 @@ class App extends Component {
       <div className="App">
         <form onSubmit={this.handleSubmit}>
           <p>
-            <input type="text" placeholder='JWT Token' id='token' value={this.state.token} onChange={this.handleChange} required/>
-          </p>
-          <p>
-            <input type="text" placeholder='Name' id='name' value={this.state.name} onChange={this.handleChange} required/>
+            <input type="text" placeholder='Title' id='name' value={this.state.name} onChange={this.handleChange} required/>
           </p>
           <p>
             <input type="file"
-                   id="image"
-                   accept="image/png, image/jpeg"  onChange={this.handleImageChange} required/>
+                   id="doc"
+                  onChange={this.handleImageChange} required/>
           </p>
           <p>
-            <select multiple={true} id='value' value={this.state.value} onChange={this.handleSelectChange}>
-              <option value="grapefruit">Grapefruit</option>
-              <option value="lime">Lime</option>
-              <option value="coconut">Coconut</option>
-              <option value="mango">Mango</option>
-            </select>
+            <input type="file"
+                   id="images"
+                  onChange={this.handleImageChange} required/>
           </p>
           <input type="submit"/>
         </form>
