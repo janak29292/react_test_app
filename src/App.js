ffa07fe3efa7e3f44dfd0328782118dd02c33890
Janak <doon.29292@gmail.com>
2019-08-27 14:26:20 +0530

    get pitch-patch

--- src/App.js
+++ src/App.js
@@ -10,10 +10,14 @@ class App extends Component {
     doc: null,
     image1: null,
     image2: null,
+    images: [],
     token: '',
     value: [],
-    topics: [],
-    cta: "interview"
+    topics: ["http://0.0.0.0:8000/api/interest/2/", "http://0.0.0.0:8000/api/interest/1/"],
+    cta: ["interview", "coverage"],
+    client: 'http://0.0.0.0:8000/api/client/1/',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    availability: 'regular'
   };
 
   handleChange = (e) => {
@@ -29,9 +33,12 @@ class App extends Component {
   }
 
   handleImageChange = (e) => {
-    this.setState({
-      [e.target.id]: e.target.files[0]
-    })
+    console.log('imgfile>>>>>>...=e', e );
+    console.log('imgfile>>>>>>', e.target.files[0] );
+    this.state.images.push(e.target.files[0])
+    // this.setState({
+    //   images: this.state.images.push(e.target.files[0])
+    // })
   };
 
   handleSubmit = (e) => {
@@ -40,16 +47,19 @@ class App extends Component {
     console.log(this.value);
     let form_data = new FormData();
     form_data.append('press_release', this.state.doc, this.state.doc.name);
-    form_data.append('images', this.state.image1, this.state.image1.name);
-    form_data.append('images', this.state.image2, this.state.image2.name);
+    for (let image of this.state.images) {
+      form_data.append('images', image, image.name);
+    }
     form_data.append('title', this.state.name);
-    form_data.append('client', 'http://0.0.0.0:8000/api/client/1/')
-    form_data.append("topics", "http://0.0.0.0:8000/api/interest/2/" )
-    form_data.append("topics", "http://0.0.0.0:8000/api/interest/1/" )
-    form_data.append("cta", "interview")
-    form_data.append("cta", "coverage")
-    form_data.append('content', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
-    form_data.append('availability', 'regular')
+    form_data.append('client', this.state.client)
+    for (let topic of this.state.topics) {
+        form_data.append("topics", topic )
+    }
+    for (let cta of this.state.cta) {
+        form_data.append("cta", cta)
+    }
+    form_data.append('content', this.state.content)
+    form_data.append('availability', this.state.availability)
     let url = 'http://localhost:8000/api/pitch/'+ this.state.id + '/';
     axios.patch(url, form_data, {
       headers: {
@@ -63,7 +73,33 @@ class App extends Component {
         .catch(err => console.log(err))
   };
 
+  handleGet = (e) => {
+    let url = 'http://localhost:8000/api/pitch/'+ this.state.id + '/';
+    axios.get(url, {
+      headers: {
+        'content-type': 'multipart/form-data',
+        'Authorization' : 'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6InByQGdtYWlsLmNvbSIsImV4cCI6MTU2ODE4ODc0MywiZW1haWwiOiJwckBnbWFpbC5jb20iLCJvcmlnX2lhdCI6MTU2NTU5Njc0M30.-FdcW4L26_xGRDhSEXRxXDxPjaEVnErJtsXrfhfwoiw'
+      }
+    })
+
+        .then(res => {
+          console.log(res.data);
+          this.setState({
+            availability: res.data.availability,
+            client: res.data.client,
+            content: res.data.content,
+            cta: res.data.cta,
+            images: res.data.images,
+            press_release: res.data.press_release,
+            title: res.data.title,
+            topics: res.data.topics
+          })
+        })
+        .catch(err => console.log(err))
+  };
+
   render() {
+    console.log("==========this.state========", this.state)
     return (
       <div className="App">
         <form onSubmit={this.handleSubmit}>
@@ -91,6 +127,9 @@ class App extends Component {
           </p>
           <input type="submit"/>
         </form>
+        <p>
+          <button onClick={this.handleGet}>Get</button>
+        </p>
       </div>
     );
   }
